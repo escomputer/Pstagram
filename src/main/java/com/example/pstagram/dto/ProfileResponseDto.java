1e85fd844d216bb875ea1829416cc8a765a0c91c
romin014 <sohyun014@naver.com>
2025-04-08 12:47:33 +0900

    Docs : Profileresponse Change delete is_deleted

--- src/main/java/com/example/pstagram/dto/ProfileResponseDto.java
+++ src/main/java/com/example/pstagram/dto/ProfileResponseDto.java
@@ -11,19 +11,16 @@ public class ProfileResponseDto {
 	private final String password;
 	private final String nickname;
 	private final String bio;
-	private final boolean is_deleted;
 	private final LocalDateTime created_at;
 	private final LocalDateTime updated_at;
 
 	public ProfileResponseDto(long id, String email, String password, String nickname, String bio,
-		boolean is_deleted,
 		LocalDateTime created_at, LocalDateTime updated_at) {
 		this.id = id;
 		this.email = email;
 		this.password = password;
 		this.nickname = nickname;
 		this.bio = bio;
-		this.is_deleted = is_deleted;
 		this.created_at = created_at;
 		this.updated_at = updated_at;
 	}
