90ff2e164325c02318f439566c697f34491631bc
escomputer <nujest307@naver.com>
2025-04-08 20:01:10 +0900

    Docs : FriendDto 정렬

--- src/main/java/com/example/pstagram/dto/dto/friend/FriendResponseDto.java
+++ src/main/java/com/example/pstagram/dto/dto/friend/FriendResponseDto.java
@@ -1,15 +1,12 @@
-package com.example.pstagram.dto.friend;
+package com.example.pstagram.dto.dto.friend;
 
 // 요청 수락 , 요청 거절 에 대한 주석
 
-import com.example.pstagram.domain.friend.FriendStatus;
-
 import lombok.AllArgsConstructor;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
+import com.example.pstagram.domain.friend.FriendStatus;
 
 @Getter
 @NoArgsConstructor
