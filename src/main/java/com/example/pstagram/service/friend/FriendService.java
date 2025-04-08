a4b8684085686426b4716028f221980e5326e2a8
escomputer <nujest307@naver.com>
2025-04-08 20:01:22 +0900

    FriendService init

---
+++ src/main/java/com/example/pstagram/service/friend/FriendService.java
@@ -0,0 +1,6 @@
+package com.example.pstagram.service.friend;
+
+public interface FriendService {
+	
+}
+
---
+++ src/main/java/com/example/pstagram/service/friend/FriendServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.pstagram.service.friend;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FriendServiceImpl implements FriendService {
+
+}
