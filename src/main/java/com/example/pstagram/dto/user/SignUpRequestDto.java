8073ec7eedc4ebd79d657937286669f74fa0ab51
bella <yiseuljeong0415@gmail.com>
2025-04-08 14:29:41 +0900

    docs : SignUpRequestDto.java 수정

--- src/main/java/com/example/pstagram/dto/user/SignUpRequestDto.java
+++ src/main/java/com/example/pstagram/dto/user/SignUpRequestDto.java
@@ -38,6 +38,8 @@ public class SignUpRequestDto {
 		regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
 		message = "비밀번호는 8자 이상, 영문/숫자/특수문자를 모두 포함해야 합니다."
 	)
+	// 정규 표현식
+
 	private String password;
 
 	/**
