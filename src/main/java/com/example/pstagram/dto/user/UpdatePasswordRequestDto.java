0bd671284d2173f59194ec421bf8fe174e4c8a71
bella <yiseuljeong0415@gmail.com>
2025-04-10 15:48:14 +0900

    refactor : 불변객체 수정

--- src/main/java/com/example/pstagram/dto/user/UpdatePasswordRequestDto.java
+++ src/main/java/com/example/pstagram/dto/user/UpdatePasswordRequestDto.java
@@ -11,13 +11,23 @@ import lombok.Getter;
 public class UpdatePasswordRequestDto {
 
 	@NotBlank(message = "현재 비밀번호는 필수입니다.")
-	private String currentPassword;
+	private final String currentPassword;
+
+
 
 	@NotBlank(message = "새 비밀번호는 필수입니다.")
 	@Pattern(
 		regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
 		message = "비밀번호는 8자 이상, 영문/숫자/특수문자를 포함해야 합니다."
 	)
-	private String newPassword;
+	private final String newPassword;
+
+	public UpdatePasswordRequestDto(String currentPassword, String newPassword) {
+		this.currentPassword = currentPassword;
+		this.newPassword = newPassword;
+	}
+
+	// fix : ㅂㅂㄱㅊ
+
 }
 
